401527b028c3a74058f711d2711d85cbc009f8fc
EmiCragnolini <ecragnolini@lavozdelinterior.com.ar>
2021-07-20 18:57:11 -0300

    Menú simple con bootstrap

---
+++ src/components/Header.js
@@ -0,0 +1,6 @@
+const Header = () =>
+    <header>
+        <h1>Hola Mundo</h1>
+    </header>
+
+export default Header
---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+const NavBar = () =>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+            <a className="navbar-brand" href="#">Vinos</a>
+            <div className="navbar-nav">
+                <a className="nav-link active" aria-current="page" href="#">Inicio</a>
+                <a className="nav-link" href="#">Link 1</a>
+                <a className="nav-link" href="#">Link 2</a>
+            </div>
+        </div>
+    </nav>
+
+export default NavBar
